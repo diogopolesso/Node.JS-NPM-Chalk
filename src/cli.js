1db920ec24668fec600a90b0b21b159a49600a05
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-05 19:54:38 -0300

    Expressoes Regulares

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,69 @@
+import chalk from 'chalk';
+import fs from 'fs';
 import pegaArquivo from './index.js';
 
 const caminho = process.argv;
 
-pegaArquivo(caminho[2]);
+function imprimimeLista(resultado, identificador = ''){
+    console.log(
+        chalk.red('Lista de Links'),
+        chalk.yellow.bgGreen(identificador),
+        resultado);
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2];
+
+    try{
+        fs.lstatSync(caminho);
+    } catch(erro){
+        if (erro.code === 'ENOENT'){
+            console.log('Arquivo ou diretório não existe');
+            return
+        }
+    }
+   
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2]);
+        console.log(chalk.red('lista de links'), resultado);
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+            arquivos.forEach(async (nomeDeArquivo) => {
+                const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+                imprimimeLista(lista, nomeDeArquivo)
+     })
+    }
+}
+
+    processaTexto(caminho);
+
+/*
+Erros comuns e como o Node.js nos avisa deles:
+
+ENOTFOUND
+Error: not found - Retorna quando o Node.js tenta estabelecer uma conexão com um servidor e a tentativa falha no DNS lookup; ou seja, ou o host não existe ou há algum erro no endereço fornecido, que não consegue ser resolvido pelo DNS.
+
+Algumas das soluções possíveis: verifique se a URL fornecida realmente existe e se a string com a URL está sendo fornecida da forma correta para a função ou método que fará a conexão.
+
+ENOENT
+Error: no entity - Como vimos no curso, muitas vezes acontece quando um caminho especificado, seja um diretório ou um arquivo, não existe no sistema de arquivos. Neste caso, temos que verificar se o caminho fornecido está correto.
+
+Importante: algumas operações com fs requerem o uso do caminho absoluto de um arquivo ou diretório. Para “montar” a string com estes caminhos, você pode utilizar uma das libs mais comuns do Node.js, a path.
+
+EISDIR
+Error: is a directory - O caminho fornecido é um diretório. Normalmente vemos este erro quando o programa esperava receber um caminho de um arquivo, mas ao invés disso recebeu um diretório. Novamente, devemos verificar a string recebida pela função ou método para conferir se está completa e montada da forma correta.
+
+ENOTDIR
+Error: not a directory - é o contrário do erro anterior, quando o programa espera receber o caminho de um diretório mas recebe o caminho de um arquivo.
+
+EADDRINUSE
+Error: address already in use - Muitas vezes este erro acontece quando estamos iniciando ou reiniciando um servidor web. Ele indica que o servidor está tentando se conectar a uma porta lógica que já está sendo ocupada por outro programa.
+
+Você pode conferir mais sobre servidores web e portas de conexão em nosso curso de fundamentos do HTTP.
+
+ECONNREFUSED
+Error: connection refused - Houve uma tentativa de envio de requisição a um endpoint, porém a conexão foi recusada. Normalmente é causada por inatividade do serviço que está sendo requisitado.
+
+ECONNRESET
+Error: connection reset - Uma conexão em andamento foi fechada durante o processo de requisição-resposta, antes que a resposta fosse recebida. Pode ser causada por um timeout ou reinício do servidor.
+*/
