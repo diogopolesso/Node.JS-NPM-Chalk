f565f283c2643ad3be2f0175d0514539807ab2a4
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-05 19:55:16 -0300

    Expressoes Regulares

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function extraiLinks(texto){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)];
     const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados;
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
 }
 
 function trataErro(erro){
@@ -23,7 +23,6 @@ async function pegaArquivo(caminhoDoArquivo){
     }
 }
 
-//pegaArquivo('./arquivos/texto.md');
 export default pegaArquivo;
 
 
