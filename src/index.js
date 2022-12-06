2c5121a1a1ee7c7bb0127d4fd5be20c46779c719
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-06 16:17:38 -0300

    Expressoes Regulares

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function extraiLinks(texto){
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = [...texto.matchAll(regex)];
-    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
-    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
+function extraiLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+  return resultados.length !== 0 ? resultados : 'não há links no arquivo';
 }
 
-function trataErro(erro){
-    console.log(erro);
-    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+function trataErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-async function pegaArquivo(caminhoDoArquivo){
-    try {
-        const enconding = 'utf-8';
-        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
-        return extraiLinks(texto);
-    }   catch(erro){
-        trataErro(erro)
-    }
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    return extraiLinks(texto);
+  } catch (erro) {
+    trataErro(erro)
+  }
 }
 
 export default pegaArquivo;
